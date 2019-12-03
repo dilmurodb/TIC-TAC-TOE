dbabb228dc351f5d09565b515c1d9e71eac26988
bukharov <dilmurod.bukharov@gmail.com>
2019-12-03 18:11:16 -0500

    js blue and red clicking works

--- TICTAC.js
+++ TICTAC.js
@@ -1,33 +1,9 @@
-// let allSquares = document.querySelectorAll(".squares")
-// allSquares.addEventListener("click", function changeColor() {
-//     console.log("hhhh")
-// })
-
-// this works but weired
-
 
 //1st want to give every square an event listener
 // 2nd want the event listener to dictate the changing of the colors based off the %
+
 const grids = document.querySelectorAll(".squares")
 let turn = 0
-//Works but wierd
-// for (let j=0; j<grids.length; j++) {
-//     grids[j].addEventListener("click", function changeColor() {
-//     for (let i=0; i<grids.length; i++) {
-//         if ( turn % 2 === 0) {
-//             grids[i].addEventListener("click", function blue() {
-//                 grids[i].style.backgroundColor = "blue";
-//                 turn++
-//             })
-//         } else {
-//             grids[i].addEventListener("click", function red() {
-//                 grids[i].style.backgroundColor = "red";
-//                 turn++
-//             })
-//         }
-//     }
-// })
-// }
 for (let i=0; i<grids.length; i++) {
     grids[i].addEventListener("click", function changeColor() {
         if (turn % 2 === 0) {
@@ -39,61 +15,3 @@ for (let i=0; i<grids.length; i++) {
         }
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i= 0; i < grids.length; i++) {
-//     if (turn % 2 === 0) {
-//         grids[i].addEventListener("click", function blue() {
-//         grids[i].style.backgroundColor = "blue"
-//         turn++
-//         console.log(turn)
-//     })} else if (turn % 2 === 1){
-//         grids[i].addEventListener("click", function red() {
-//             grids[i].style.backgroundColor = "red"
-//             console.log(turn)
-//     })
-    
-//     }
-// }
-
-
-// grids[i].addEventListener("click", function blue() {
-//     for (let i= 0; i < grids.length; i++) {
-//         if (turn % 2 === 0) {
-
-//             grids[i].style.backgroundColor = "blue"
-//             turn++
-//         })} else if (turn % 2 === 1){
-//             grids[i].addEventListener("click", function red() {
-//                 grids[i].style.backgroundColor = "red"
-//                 console.log(turn)
-//         })
-        
-//         }
-
-//     }
-// })
-
-
-
-
-// const grids = document.querySelectorAll(".squares")
-// for (let i=0; i < grids.length; i++) {
-//     grids[i].addEventListener("click", function() {
-//         grids[i].style.backgroundColor = "blue"
-//     })
-// }
