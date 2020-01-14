9f3da735553b817f09fa50abdc3aaf23df139964
bukharov <dilmurod.bukharov@gmail.com>
2020-01-13 22:15:21 -0500

    removed commented out code

--- TICTAC.js
+++ TICTAC.js
@@ -1,6 +1,3 @@
-//1st want to give every square an event listener
-// 2nd want the event listener to dictate the changing of the colors based off the %
-
 const grids = document.querySelectorAll(".squares");
 let turn = 0;
 for (let i = 0; i < grids.length; i++) {
@@ -60,8 +57,6 @@ for (let i = 0; i < grids.length; i++) {
         ) {
           alert("blue won");
         }
-        // let blueArray =[];
-        // blueArray.push(i);
       } else {
         turn--;
       }
@@ -124,7 +119,6 @@ for (let i = 0; i < grids.length; i++) {
       } else {
         turn--;
       }
-      // grids[i].style.backgroundColor = "red";
       turn++;
     }
   });
@@ -136,12 +130,3 @@ button.addEventListener("click", function resetGame() {
     grids[i].style.backgroundColor = "white";
   }
 });
-
-// let array1 = [0, 1, 2]
-// let array2 = [3, 4, 5]
-// let array3 = [6, 7, 8]
-// let array4 = [0, 3, 6]
-// let array5 = [1, 4, 7]
-// let array6 = [2, 5, 8]
-// let array7 = [0, 4, 8]
-// let array8 = [2, 4, 6]
